cdd287bf174d39b4f9366536f4e99743da013f98
Carolina <ctorresc@uqvirtual.edu.co>
2021-11-03 00:54:31 -0500

    se eliminó los overwritte

--- src/interfaz/VentanaInicio.java
+++ src/interfaz/VentanaInicio.java
@@ -88,7 +88,7 @@ public class VentanaInicio extends JFrame implements ActionListener {
 		
 	}
 	
-	@Override
+	
 	public void actionPerformed(ActionEvent e) {
 		
 		if(e.getSource() == btnIngresar) {	
--- src/interfaz/VentanaLogin.java
+++ src/interfaz/VentanaLogin.java
@@ -119,7 +119,7 @@ public class VentanaLogin extends JFrame implements ActionListener {
 				
 	}
 	
-	@Override
+	
 	public void actionPerformed(ActionEvent e) {
 		
 		if(e.getSource() == btnIngresar) {	
